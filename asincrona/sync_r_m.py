157f42efb55bd0944b1de9772b725433a37d2b3c
LeninElio <moreno_leninelio@hotmail.es>
2023-05-01 08:11:31 -0500

    Eliminar innecesarios

--- asincrona/sync_r_m.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://rickandmortyapi.com/api/character/{}'
-
-for personaje in range(1, 10):
-    response = requests.get(url.format(personaje))
-    data = response.json()
-    print(data['name'])
--- test_functions/sql_test.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append(".")
-from funciones import sql
-
-data = sql.lista_query('select top 100 * from dbo.Rendimiento')
-
-print(data)
